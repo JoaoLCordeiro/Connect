c914fc68f003c92c856459c9e38c21781535ef2f
JoaoLCordeiro <joaolucascordeiro@gmail.com>
2020-08-22 15:54:18 -0300

    jogo finalizado brabo

--- connect.c
+++ connect.c
@@ -2,6 +2,7 @@
 #include    <stdlib.h>
 #include    <ncurses.h>
 #include    <string.h>
+#include    <time.h>
 
 #define     NADA        0
 #define     NMEXEGIRA   1
@@ -49,7 +50,7 @@ void faz_fase (t_tabuleiro *tab)
     tab->m[0][0]->dir  = 3;
 
     tab->m[1][2]->tipo = MEXEGIRA;
-    tab->m[1][2]->cima = 0;
+    tab->m[1][2]->cima = 2;
     tab->m[1][2]->baixo= 0;
     tab->m[1][2]->esq  = 1;
     tab->m[1][2]->dir  = 0;
@@ -59,6 +60,12 @@ void faz_fase (t_tabuleiro *tab)
     tab->m[3][3]->baixo= 3;
     tab->m[3][3]->esq  = 0;
     tab->m[3][3]->dir  = 0;
+
+    tab->m[2][1]->tipo = NMEXEGIRA;
+    tab->m[2][1]->cima = 0;
+    tab->m[2][1]->baixo= 0;
+    tab->m[2][1]->esq  = 0;
+    tab->m[2][1]->dir  = 2;
 }
 
 int verifica_tabuleiro (t_tabuleiro *tab)
@@ -291,6 +298,152 @@ void gira_peca (t_tabuleiro *tab,int linha,int coluna)
     tab->m[linha][coluna]->dir   = sup;
 }
 
+void algoritmo_cria_fase (t_tabuleiro *tab)
+{
+    int chance,i,j;
+    for (i=0 ; i<4 ; i++)
+    {
+        for (j=0 ; j<4 ; j++)
+        {   
+            if (i != 0)
+            {
+                if (tab->m[i-1][j]->baixo != 0)
+                    tab->m[i][j]->cima = tab->m[i-1][j]->baixo;
+            }
+            if (j != 0)
+            {
+                if (tab->m[i][j-1]->dir != 0)
+                    tab->m[i][j]->esq = tab->m[i][j-1]->dir;
+            }
+            if (i != 3)
+            {
+                chance = rand() % 3;
+                if (chance == 1) /* 33% de chance */
+                    tab->m[i][j]->baixo = (rand() % 4) + 1;
+            }
+            if (j != 3)
+            {
+                chance = rand() % 3;
+                if (chance == 1) /* 33% de chance */
+                    tab->m[i][j]->dir = (rand() % 4) + 1;
+            }
+            if ((tab->m[i][j]->baixo != 0) || (tab->m[i][j]->cima != 0) || (tab->m[i][j]->dir != 0) || (tab->m[i][j]->esq != 0))
+            {
+                chance = (rand() % 10);
+                if (chance < 2)
+                    tab->m[i][j]->tipo = 1;
+                else if ((chance > 1)&&(chance <4))
+                    tab->m[i][j]->tipo = 2;
+                else if ((chance > 3)&&(chance <6))
+                    tab->m[i][j]->tipo = 3;
+                else
+                    tab->m[i][j]->tipo = 4;
+            }
+        }
+    }
+}
+
+void troca_peca (t_tabuleiro *tab, int lin1, int col1, int lin2, int col2)
+{
+    int sup;
+    sup = tab->m[lin1][col1]->tipo;
+    tab->m[lin1][col1]->tipo = tab->m[lin2][col2]->tipo;
+    tab->m[lin2][col2]->tipo = sup;   
+    sup = tab->m[lin1][col1]->cima;
+    tab->m[lin1][col1]->cima = tab->m[lin2][col2]->cima;
+    tab->m[lin2][col2]->cima = sup;                     
+    sup = tab->m[lin1][col1]->baixo;
+    tab->m[lin1][col1]->baixo = tab->m[lin2][col2]->baixo;
+    tab->m[lin2][col2]->baixo = sup;                     
+    sup = tab->m[lin1][col1]->dir;
+    tab->m[lin1][col1]->dir = tab->m[lin2][col2]->dir;
+    tab->m[lin2][col2]->dir = sup;                     
+    sup = tab->m[lin1][col1]->esq;
+    tab->m[lin1][col1]->esq = tab->m[lin2][col2]->esq;
+    tab->m[lin2][col2]->esq = sup;                     
+}
+
+void embaralha_fase (t_tabuleiro *tab)
+{
+    int i,j,k,n;
+    int lin,col;
+    for (i=0 ; i<4 ; i++)
+    {
+        for (j=0 ; j<4 ; j++)
+        {
+            if ((tab->m[i][j]->tipo == GIRA) || (tab->m[i][j]->tipo == MEXEGIRA))
+            {
+                n = rand() % 3 + 1;
+                for (k=0 ; k<n ; k++)
+                {
+                    gira_peca (tab,i,j);
+                } 
+            }
+            if ((tab->m[i][j]->tipo == MEXE) || (tab->m[i][j]->tipo == MEXEGIRA))
+            {
+                n = rand() % 5;
+                if (n!=4) /*80% de chance de mover a peca*/
+                {
+                    lin = rand() % 4;
+                    col = rand() % 4;
+                    while (!((tab->m[lin][col]->tipo == MEXE) || (tab->m[lin][col]->tipo == MEXEGIRA) || (tab->m[lin][col]->tipo == NADA)))
+                    {
+                        lin = rand() % 4;
+                        col = rand() % 4;
+                    }
+                    troca_peca (tab,i,j,lin,col);
+                }
+            }
+        }
+    }
+}
+
+void imprime_menu (WINDOW *wtab)
+{
+    werase(wtab);
+    int lin, col;
+    lin = 10;
+    col = 10;
+    box(wtab,0,0); 
+    mvwprintw (wtab, lin  , col, "  _____                            _   ");
+    mvwprintw (wtab, lin+1, col, " / ____|                          | |  ");
+    mvwprintw (wtab, lin+2, col, "| |     ___  _ __  _ __   ___  ___| |_ ");
+    mvwprintw (wtab, lin+3, col, "| |    / _ \\| '_ \\| '_ \\ / _ \\/ __| __|");
+    mvwprintw (wtab, lin+4, col, "| |___| (_) | | | | | | |  __/ (__| |_ ");
+    mvwprintw (wtab, lin+5, col, " \\_____\\___/|_| |_|_| |_|\\___|\\___|\\__|");
+
+    mvwprintw (wtab, lin+10, col   , "Pecas ");
+    wattron (wtab,COLOR_PAIR(1));
+    mvwprintw (wtab, lin+10, col+6 , "vermelhas ");
+    wattroff(wtab,COLOR_PAIR(1));
+    mvwprintw (wtab, lin+10, col+16, "nao mexem, nem giram");
+
+    mvwprintw (wtab, lin+11, col   , "Pecas ");
+    wattron (wtab,COLOR_PAIR(2));
+    mvwprintw (wtab, lin+11, col+6 , "azuis ");
+    wattroff(wtab,COLOR_PAIR(2));
+    mvwprintw (wtab, lin+11, col+16, "somente giram");
+
+    mvwprintw (wtab, lin+12, col   , "Pecas ");
+    wattron (wtab,COLOR_PAIR(3));
+    mvwprintw (wtab, lin+12, col+6 , "verdes ");
+    wattroff(wtab,COLOR_PAIR(3));
+    mvwprintw (wtab, lin+12, col+16, "somente mexem");
+
+    mvwprintw (wtab, lin+13, col   , "Pecas ");
+    wattron (wtab,COLOR_PAIR(4));
+    mvwprintw (wtab, lin+13, col+6 , "laranjas ");
+    wattroff(wtab,COLOR_PAIR(4));
+    mvwprintw (wtab, lin+13, col+16, "giram e mexem");
+
+    mvwprintw (wtab, lin+15, col, "Conecte todos os pares de numeros iguais");
+    mvwprintw (wtab, lin+16, col, "e conclua o nível!                      ");
+
+    mvwprintw (wtab, lin+18, col, "Aperte espaco para comecar!");
+    mvwprintw (wtab, lin+19, col, "Aperte q para fechar o jogo.");
+    wrefresh(wtab);
+}
+
 int main ()
 {
     initscr();
@@ -317,38 +470,72 @@ int main ()
     linha2  = 0;
     coluna2 = 0;
     char tecla;
+    tecla = 'o';
 
-    int movendo = 0;
-
-    faz_fase (&tabuleiro);      /*retirado na versão final, apenas para testes*/
-    
-    while (! (verifica_tabuleiro (&tabuleiro)))
+    while (tecla != 'q')
     {
-            if (! movendo)
+        imprime_menu (wtabuleiro);
+        nodelay(stdscr, TRUE);
+        while ((tecla != ' ')&&(tecla != 'q'))
+        {   
+            imprime_menu (wtabuleiro);
+            tecla = getch();
+        }
+        nodelay (stdscr, FALSE);
+    
+        if (tecla == ' ')
+        {
+            int movendo = 0;
+            srand(time(0));
+
+            algoritmo_cria_fase (&tabuleiro);
+            embaralha_fase (&tabuleiro);
+    
+            while (!(verifica_tabuleiro (&tabuleiro))) /*verifica se o jogo acabou*/
             {
-                imprime_tabuleiro(wtabuleiro,&tabuleiro);   
-                imprime_select(wtabuleiro,linha1,coluna1);
-                tecla = getch();
-                mover_select (tecla,&linha1,&coluna1);
-                if ((tecla == 'j')&&((tabuleiro.m[linha1][coluna1]->tipo == MEXE)||(tabuleiro.m[linha1][coluna1]->tipo == MEXEGIRA)))
-                    movendo = 1;
-                if ((tecla == 'k')&&((tabuleiro.m[linha1][coluna1]->tipo == GIRA)||(tabuleiro.m[linha1][coluna1]->tipo == MEXEGIRA)))
-                    gira_peca (&tabuleiro,linha1,coluna1);
-            }
-            else
-            {
-                imprime_tabuleiro(wtabuleiro,&tabuleiro);                  
-                imprime_select(wtabuleiro,linha1,coluna1);
-                imprime_select2(wtabuleiro,linha2,coluna2);
-                tecla = getch();
-                mover_select (tecla,&linha2,&coluna2);
-                if (tecla == 'j')
+                if (! movendo)                              /*parte que executa quando não estamos movendo uma peça*/
                 {
-                    if (tabuleiro.m[linha2][coluna2]->tipo == NADA)
-                        move_peca (&tabuleiro,linha1,coluna1,linha2,coluna2);
-                    movendo = 0;
+                    imprime_tabuleiro(wtabuleiro,&tabuleiro);   
+                    imprime_select(wtabuleiro,linha1,coluna1);
+                    wrefresh(wtabuleiro);
+                    refresh();
+                    tecla = getch();
+                    mover_select (tecla,&linha1,&coluna1);
+                    if ((tecla == 'j')&&((tabuleiro.m[linha1][coluna1]->tipo == MEXE)||(tabuleiro.m[linha1][coluna1]->tipo == MEXEGIRA)))
+                    {
+                        movendo = 1;
+                        linha2 = linha1;
+                        coluna2 = coluna1;
+                    }
+                    if ((tecla == 'k')&&((tabuleiro.m[linha1][coluna1]->tipo == GIRA)||(tabuleiro.m[linha1][coluna1]->tipo == MEXEGIRA)))
+                        gira_peca (&tabuleiro,linha1,coluna1);
+                }   
+                else                                        /*parte que executa quando estamosmovendo uma peça*/
+                {
+                    imprime_tabuleiro(wtabuleiro,&tabuleiro);                  
+                    imprime_select(wtabuleiro,linha1,coluna1);
+                    imprime_select2(wtabuleiro,linha2,coluna2);
+                    tecla = getch();
+                    mover_select (tecla,&linha2,&coluna2);
+                    if (tecla == 'j')
+                    {
+                        if (tabuleiro.m[linha2][coluna2]->tipo == NADA)
+                            move_peca (&tabuleiro,linha1,coluna1,linha2,coluna2);
+                        movendo = 0;
+                        linha1 = linha2;
+                        coluna1 = coluna2;
+                    }
                 }
             }
+        
+            imprime_tabuleiro(wtabuleiro,&tabuleiro);
+            mvwprintw (wtabuleiro, 0, 2, "Parabens, voce concluiu a fase! Aperte j pra continuar!");
+            wrefresh(wtabuleiro);
+            tecla = getch();
+            while (tecla != 'j')
+                tecla = getch();
+            wclear(wtabuleiro);
+        }
     }
     endwin();
     return 0;
